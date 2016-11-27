4ec9163492cabcd8ba2f6c254a94d1699db98e3d
cwhitman <calebrwhitman@gmail.com>
2016-11-26 22:40:19 -0700

    Make some progess with RedditSharp
    RedditSharp still cannot use Oath2 to retrieve information. I was able
    to get it to connect with Reddit via Oath2 though.

--- Source/UnityDemo/Assets/Scripts/RedditSharpTest.cs
+++ Source/UnityDemo/Assets/Scripts/RedditSharpTest.cs
@@ -8,28 +8,32 @@
  * The code for RedditSharp was built into a library and put into Plugs/Resources. 
  * 
  * Currently, I am able to get RedditSharp to work if you pass in a Redditor's Username and Password.
- * I am not able to get RedditSharp to work with Oath2. Theoretically it should be simple, but all of my tests so far have failed.
- * Right now, I believe that I am using the various classes/methods innocorretly. 
- * The function of every class and method is documented (for the most part) but it is not completely clear how to put all of the various pieces together.
+ * I am not able to get RedditSharp to work with Oath2. 
+ * The code successfuly obtains an access token but then crashes when trying to instantiate a Reddit object.
  */
 
 using UnityEngine;
 using System.Collections;
+using Stopwatch=System.Diagnostics.Stopwatch;
 using RedditSharp;
+using Scope =RedditSharp.AuthProvider.Scope;
 using UnityEditor;
-//ignoring variable not used warnings.
-#pragma warning disable 414, 168 
+
+
+#pragma warning disable 414, 168 //ignoring "variable not used" warnings.
 public class RedditSharpTest  {
 
 
+	//enter the app secret here
+	private static string 	app_secret="";
 
-	private static string app_secret="";
-	private static string user_agent = "User-Agent: UwyoSenDesign2017:v0.0.1 (by /u/mcook42)";
+	private static string 	user_agent = "User-Agent: UwyoSenDesign2017:v0.0.1 (by /u/mcook42)";
 	private static string	app_id = "DNvP_RE1N9NqQg";
 	private static string	app_uri = "https://127.0.0.1:65010/authorize_callback";
-	private static string	app_scopes = "account creddits edit flair history identity livemanage modconfig modcontributors modflair modlog modothers modposts modself modwiki mysubreddits privatemessages read report save submit subscribe vote wikiedit wikiread";
-	private static string	app_code = "3cV6G9Op2_3wVbXzOnU9NgEz8ns";
+	private static string	app_code = "KxpXC7clBaDYsBPU1E3JJlq77j8";
 	private static string	app_refresh = "64281718-kVkekkFsh-YOeFKUmqlpZF0lCkE";
+	private static string 	app_state="533swmzldjwu9a5"; 
+	private static Scope 	app_scopes = Scope.edit | Scope.flair | Scope.history | Scope.identity |Scope.modconfig |Scope.modflair | Scope.modlog| Scope.modposts | Scope.modwiki|Scope.mysubreddits | Scope.privatemessages |Scope.read|Scope.report|Scope.save|Scope.submit|Scope.subscribe|Scope.vote|Scope.wikiedit|Scope.wikiread;
 
 	//enter your own username and password here.
 	private static string 	username="";
@@ -45,7 +49,10 @@ public class RedditSharpTest  {
 		}
 		try{
 
+
 			var reddit = new Reddit(username,password);
+			var timer =new Stopwatch();
+			timer.Start();
 
 			var subreddit = reddit.RSlashAll;
 			int stop=25;
@@ -57,15 +64,54 @@ public class RedditSharpTest  {
 				stop--;
 			}
 
+			timer.Stop();
+			Debug.Log("Seconds taken: " + (timer.ElapsedMilliseconds/1000) );
+
+
 
 			//Code that does not work.
-			//This code is coppied directly from the github page (in Program.cs), yet it fails to login.
-			//Either I am using it wrong or RedditSharp does not possess the capabilites to do what I want it to do.
-			//var webAgent=new WebAgent();
-			//WebAgent.UserAgent=user_agent;
-			//var authProvider = new AuthProvider (app_id,app_secret, app_uri,webAgent);
+			//This code is coppied almost directly from the github page (in Program.cs)
+			//The code throws a NullPointerException when the Reddit object is encountered
+
+			/*
+
+			if (app_secret=="") {
+				Debug.Log ("You need to enter the app_secret in the script scripts/RedditSharpTest.cs\nRemove this upon uploading to github.");
+				return;
+			}
+			var webAgent=new WebAgent();
+			WebAgent.UserAgent=user_agent;
+			var authProvider = new AuthProvider (app_id,app_secret, app_uri,webAgent);
+
+			//copy and paste this url into a browser. Click accept. Copy the code in the returned url and use it as the app_code.
+			//var authURL=authProvider.GetAuthUrl(app_state,app_scopes,true); Debug.Log(authURL);
+
+			var authToken = authProvider.GetOAuthToken(app_code); Debug.Log(authToken);
+
+			
+			var reddit = new Reddit(authToken);//code crashes right here.
+
+
+			var timer =new Stopwatch();
+			timer.Start();
+
+			var subreddit = reddit.RSlashAll;
+			int stop=25;
+			foreach(var post in subreddit.Hot)
+			{
+				Debug.Log(post.Subreddit.Name);
+				if(stop<=0)
+					break;
+				stop--;
+			}
+
+			timer.Stop();
+			Debug.Log("Seconds taken: " + (timer.ElapsedMilliseconds/1000) );
+
 			//var authTokenString = authProvider.GetOAuthToken(username, password);
-		
+
+*/
+	
 	
 
 		}
