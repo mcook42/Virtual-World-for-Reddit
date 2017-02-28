5d7c9a5f18a0fbe54f90bd6c342ea3311c581728
mcook42 <speedymaco@gmail.com>
2017-02-28 15:07:27 -0700

    csvToDatabase is fully functional
    It will now clean up the data and insert only valid entries into the
    database. Tested on small test database. Still need to test on server.

--- Source/PRAW/csvToDatabase.py
+++ Source/PRAW/csvToDatabase.py
@@ -15,7 +15,7 @@ __version__ = "0.0.1"
 __email__ = "mattheworion.cook@gmail.com"
 
 
-def clean_data(table_struct, filename):
+def clean_data(table_struct, filename, cur):
     """Cleans unecessary data from table and converts empty entries to NULL"""
     # NOTE: If you run this twice, you WILL DUPLICATE THE DUPLICATES OF YOUR FILES.
     # BigQuery has no null representation, so go through and add NULL
@@ -23,15 +23,25 @@ def clean_data(table_struct, filename):
     namez = ["ï»¿created_utc", "subreddit", "subreddit_id", "author", "domain", "num_comments",
                                  "score", "title", "selftext", "full_name", "gilded", "over_18", "thumbnail", "is_self",
                                  "from_id", "permalink", "distinguished"]
-    try:
-        df = pandas.read_csv(filename, header=0, low_memory=False, quoting=1,
-                             usecols=[0, 1, 2, 3, 5, 6, 9, 10, 14, 18, 19, 20, 25, 27, 28, 32])
-    except Exception as e:
-        print("error: ", e)
-    df.rename(columns={'name': 'full_name', 'ï»¿created_utc': "created_utc"}, inplace=True)
-    df.dropna(subset=["subreddit"], inplace=True)
+
     newname = filename.replace(".csv", "-new.csv")
     if table_struct is "posts":
+        try:
+            df = pandas.read_csv(filename, header=0, low_memory=False, quoting=1,
+                                 usecols=[0, 1, 2, 3, 5, 6, 9, 10, 14, 18, 19, 20, 25, 27, 28, 32])
+        except Exception as e:
+            print("error: ", e)
+        df.rename(columns={'name': 'full_name', 'ï»¿created_utc': "created_utc"}, inplace=True)
+
+        # Drop entries where the subreddit doesn't exist in our subreddit database.
+        # This should never be the case on the server side, but we still need to do it to ignore
+        # unnecessary data and prevent issues with the COPY FROM function.
+        valid_names = dbInteractions.get_subreddit_ids(cur)
+        df = df[df.subreddit_id.isin(valid_names)]
+
+        # Drop entries where no subreddit is specified. We do not need this data.
+        df.dropna(subset=["subreddit"], inplace=True)
+
         # Write to file
         df.to_csv(newname, sep=',',
                   # columns=(("created_utc", "subreddit", "subreddit_id", "author", "domain", "num_comments",
@@ -48,7 +58,7 @@ def clean_data(table_struct, filename):
 
 
 def main():
-    directory = sys.argv[1]
+    directory = "C:\\Users\\Someone\\Desktop\\testDir" #sys.argv[1]
 
     if not directory:
         print("Please provide the directory of the file(s) to be read into the db.\n")
@@ -61,16 +71,15 @@ def main():
     files = os.listdir(directory)
 
     for filename in files:
-        # if "comments" in filename:
-        #     new_filename = clean_data(table_struct="comments", filename=filename)
-        # elif "post" in filename:
-        new_filename = clean_data(table_struct="posts", filename=filename)
+        # Open connection and cursor for the insertion of the table
+        conn = dbInteractions.open_conn()
+        cur = conn.cursor()
+        if "comments" in filename:
+             new_filename = clean_data(table_struct="comments", filename=filename, cur=cur)
+        elif "post" in filename:
+            new_filename = clean_data(table_struct="posts", filename=filename, cur=cur)
 
         with open(new_filename, 'r', encoding='utf-8') as infile:
-            # Open connection and cursor for the insertion of the table
-            conn = dbInteractions.open_conn()
-            cur = conn.cursor()
-
             # Check which table it's supposed to go into
             if "post" in filename:
                 query = "COPY reddit.post FROM %s CSV HEADER"
